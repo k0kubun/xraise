b976a0682c223ad97c48af1a025baacb8fd884a0
Takashi Kokubun <takashikkbn@gmail.com>
2017-01-03 21:14:42 +0900

    Get window list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+extern crate x11;
+
+use std::ffi::CString;
+use std::mem::zeroed;
+use std::ptr;
+use x11::xlib::*;
+
+fn intern_atom(display: *mut Display, name: &str) -> u64 {
+    unsafe {
+        let cstr = CString::new(name).unwrap();
+        XInternAtom(display, cstr.as_ptr(), False)
+    }
+}
+
+fn get_property(display: *mut Display,
+                window: Window,
+                prop_name: &str,
+                size: *mut u64)
+                -> *mut i64 {
+    unsafe {
+        let mut actual_type: u64 = 0;
+        let mut actual_format: i32 = 0;
+        let mut n_items: u64 = 0;
+        let mut bytes_after: u64 = 0;
+        let mut prop: *mut u8 = zeroed();
+
+        let net_client_list = intern_atom(display, prop_name);
+        XGetWindowProperty(display,
+                           window,
+                           net_client_list,
+                           0,
+                           1024,
+                           False,
+                           AnyPropertyType as u64,
+                           &mut actual_type,
+                           &mut actual_format,
+                           &mut n_items,
+                           &mut bytes_after,
+                           &mut prop);
+
+        if n_items > 0 {
+            *size = n_items;
+            prop as *mut i64
+        } else {
+            panic!("XGetWindowProperty failed")
+        }
+    }
+}
+
+fn get_client_list(display: *mut Display, size: *mut u64) -> *mut Window {
+    unsafe {
+        let root = XDefaultRootWindow(display);
+        let client_list = get_property(display, root, "_NET_CLIENT_LIST", size);
+        client_list as *mut Window
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    unsafe {
+        let display = XOpenDisplay(ptr::null());
+        if display.is_null() {
+            panic!("XOpenDisplay failed")
+        }
+
+        let mut size = 0;
+        let windows = get_client_list(display, &mut size);
+
+        for i in 0..size {
+            let window = windows.offset(i as isize);
+            println!("hello");
+        }
+
+        XCloseDisplay(display);
+    }
 }
